497b288b6783e222c1ec1dee1fddc7d6d2fce5ff
neulii <neulii@gmx.at>
2019-10-18 06:48:26 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+public class Game {
+
+    public int width = 300;
+    public int height = width /16 * 9;
+
+    public int scale = 3;
+
+    public static void main(String[] args){
+
+
+
+    }
+
+
+}
