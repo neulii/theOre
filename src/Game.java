304041e21658363ebbb44f52959b298ad1cbf170
neulii <neulii@gmx.at>
2019-10-18 06:56:43 +0200

    threads ar ok

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,38 @@
-public class Game {
+import java.awt.*;
 
-    public int width = 300;
-    public int height = width /16 * 9;
+public class Game extends Canvas implements Runnable {
+
+    private int width = 300;
+    private int height = width /16 * 9;
+
+    private boolean running = false;
 
     public int scale = 3;
+
+    Thread thread;
+
+    public void start(){
+        thread = new Thread(this, "Display");
+        thread.start();
+
+    }
+
+    public void stop(){
+        try{
+            thread.join();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+        while(running){
+
+        }
+
+    }
+
 
     public static void main(String[] args){
 
